11e1d96e22a01647a19e64d57cb605cfafe7b2eb
Max Salminen <cheduic@gmail.com>
2016-01-12 11:42:18 +0200

    Include uploader in image data mock

--- src/mu_loader/components/images.clj
+++ src/mu_loader/components/images.clj
@@ -4,14 +4,16 @@
   ;; Return an array of images based on query parameters
   [{:id -1
     :description "Placeholder image"
+    :uploader "Nick"
     :img-url "/data/images/test.jpg"
     :date "11/01/2016"
-    :rank -1}])
+    :rating -1}])
 
 (defn get-image [id]
   ;; Return info for specific image
   {:id id
    :description "Placeholder image"
+   :uploader "Nick"
    :img-url "/data/images/test.jpg"
    :date "11/01/2016"
-   :rank -1})
+   :rating -1})
